be50617fe8968a54e68aaac952748ce2102c106b
henriquepablo <pablo269704@gmail.com>
2022-12-14 02:17:56 -0300

    fazendo a verificação de login do usuário

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,56 @@
+let mensagem = '';
+let type = '';
+
+const user = require('../database/models/user');
+
 exports.renderLogin = (req, res) => {
-    res.render('login');
+    setTimeout(() => {
+        mensagem = ''
+    }, 1000);
+    res.render('login', {mensagem, type});
 }
 
-exports.login = (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+exports.login = async(req, res) => {
+    
+    const {email, password} = req.body;
+
+    const login = await user.findOne({where: {email}});
+
+    if(!email || !password) {
+        mensagem = 'Campo email ou senha está em branco';
+        
+        type = 'atencao'
+        
+        console.log('Campo email ou senha está em branco');
+        res.redirect('/');
+    }
+
+    else if(!login) {
+        mensagem = 'Usuário não encontrado'
+        
+        type = 'insucesso'
+        
+        console.log('Usuário não encontrado');
+        res.redirect('/');
+    }
+
+    else if(!login.authenticate(password)) {
+        mensagem = 'Email ou senha incorreto';
+
+        type = 'atencao';
+
+        console.log('Email ou senha incorreto');
+        res.redirect('/');
+    }
+
+    else {
+        mensagem = 'Logado com sucesso';
+
+        type = 'sucesso';
+
+        console.log('logado com sucesso');
+
+        res.redirect('/');
+    }
+
 }
