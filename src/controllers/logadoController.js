62baea31db8a6b8e1408d3d2183c81f7b0577db7
henriquepablo <pablo269704@gmail.com>
2022-12-23 01:43:58 -0300

    mostrando as imagens baixadas

--- src/controllers/logadoController.js
+++ src/controllers/logadoController.js
@@ -1,5 +1,9 @@
+const imgModel = require('../database/models/imageModel');
+
 exports.logado = (req, res) => {
-    res.render('logado', {mensagem: ''});
+    imgModel.find({}, (err, items) => {
+        res.render('logado', {mensagem: '', items: items}); 
+    });
 }
 
 exports.logout = (req, res) => {
