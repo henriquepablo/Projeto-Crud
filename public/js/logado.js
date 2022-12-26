dad82b568304d84c6db066791c6a7d56795aaafe
henriquepablo <pablo269704@gmail.com>
2022-12-26 16:54:13 -0300

    efeito de carregamento

---
+++ public/js/logado.js
@@ -0,0 +1,4 @@
+const livro = document.querySelector('.cadastroLivro').addEventListener('click', () => {
+    document.querySelector('.modal').style.display = 'none';
+    document.querySelector('.loader').style.display = 'block';
+})
